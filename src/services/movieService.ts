5aec53cc1fc15cb5d643eec337be73a88b149c55
Nadya <studenikina.nadya@gmail.com>
2026-05-02 20:55:18 +0200

    feil modal and fetch

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-interface GetMovieresResponse {
+interface GetMoviesResponse {
   results: Movie[];
 }
 
 export default async function fetchMovies(name: string): Promise<Movie[]> {
-  const response = await axios.get<GetMovieresResponse>(
+  const response = await axios.get<GetMoviesResponse>(
     `https://api.themoviedb.org/3/search/movie?query=${name}`,
     {
-      params: {},
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
       },
